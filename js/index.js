70a1cbdcaf0b101a536c4636ac3ab21322a85aa5
Artyom Krasyuk <artem.krasyuk2004@gmail.com>
2026-02-06 21:21:58 +0400

    main page and movie

--- js/index.js
+++ js/index.js
@@ -1,41 +1,85 @@
 'use strict'
 
-class Movie{
-    constructor(movieId, title, genres, duration, dates, img){
-        this.movieId = movieId;
-        this.title = title;
-        this.genres = genres;
-        this.duration = duration;
-        this.dates = dates;
-        this.img = img;
+class Showtime{
+    constructor(movie, time){
+        this.movie = movie; // объект Movie
+        this.time = time; // список времён сеансов
     }
 }
 
-let genres = ["Анимация", "Биография", "Боевик", "Драма", "Комедия", "Научная фантастика"];
+class Movie{
+    constructor(movieId, title, genres, duration, poster, description){
+        this.movieId = movieId;
+        this.title = title;
+        this.genres = genres; // список строк
+        this.duration = duration;
+        this.poster = poster;
+        this.description = description;
+    }
+}
 
-let movies = [
-    new Movie(
-        1, "Дюна: Часть вторая", ["Научная фантастика"], 168, ["2026-01-10"], "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?auto=format&fit=crop&w=400&q=80"
-    ),
-    new Movie(
-        2, "Дюна: Часть вторая", ["Боевик"], 168, ["2026-01-11"], "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?auto=format&fit=crop&w=400&q=80"
-    )
-];
+let port = 44249;
 
-let dates = ["2026-01-10", "2026-01-11"];
+let showtimesByMovie = new Map();
+let uniqueGenres = new Set();
+let uniqueTimes = new Set();
 
-function getMovies(){
-    const pickr = window.flatpickr('#date__value', {
+async function getShowtimes(){
+    let url = `http://127.0.0.1:${port}/api/showtimes/movies`;
+    let response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        credentials: 'include'
+    });
+    if(response.ok){
+        let body = await response.text();
+        let json = JSON.parse(body);
+        showtimesByMovie.clear();
+        json.forEach(item => {
+            const movie = new Movie(
+            parseInt(item.movie.movieId),
+            item.movie.title,
+            item.movie.genres,
+            item.movie.duration,
+            item.movie.poster,
+            item.movie.description
+            );
+
+            item.movie.genres.forEach(genre => uniqueGenres.add(genre));
+            uniqueTimes.add(item.time.split(' ')[0]);
+
+            if (showtimesByMovie.has(movie.movieId)) {
+                const existingShowtime = showtimesByMovie.get(movie.movieId);
+                if (!existingShowtime.time.includes(item.time.split(' ')[0])) {
+                    existingShowtime.time.push(item.time.split(' ')[0]);
+                }
+            } else {
+                const showtime = new Showtime(
+                    movie,
+                    [item.time.split(' ')[0]]
+                );
+                showtimesByMovie.set(movie.movieId, showtime);
+            }
+        });
+
+        setShowtimes();
+        setGenres();
+
+        const pickr = window.flatpickr('#date__value', {
         'locale': 'ru',
-        enable: dates,
+        enable: [...uniqueTimes],
         dateFormat: 'd.m.Y'
     });
 
-    document.querySelector('#date__value').onchange = setMovies;
+    document.querySelector('#date__value').onchange = setShowtimes;
     document.querySelector('.date__reset').onclick = pickr.clear;
+    }
+    else alert('Ошибка получения киносеансов');
 }
 
-function setMovies(){
+function setShowtimes(){
     let container = document.querySelector('.movies__container');
     container.innerHTML = '';
     let date = document.querySelector('#date__value').value;
@@ -45,21 +89,21 @@ function setMovies(){
     }
     let genre = document.querySelector('.active_genre').innerHTML;
     let count = 0;
-    for(let key in movies){
-        if(genre != 'Все жанры' && !movies[key].genres.includes(genre))continue;
-        if(date != '' && !movies[key].dates.includes(date))continue;
+    for(const [key, value] of showtimesByMovie){
+        if(genre != 'Все жанры' && !value.movie.genres.includes(genre))continue;
+        if(date != '' && !value.time.includes(date))continue;
 
         let str = 
         `
         <div class="movie">
-            <img class="movie__img" src="${movies[key].img}">
+            <img class="movie__img" src="${value.movie.poster}">
             <div class="movie__body">
                 <div class="movie_desc">
-                    <div class="movie__title">${movies[key].title}</div>
-                    <div class="movie__genre">${movies[key].genres.join(', ')}</div>
-                    <div class="movie__duration"><img src="img/clock.png" alt=""><div class="duration__text">${movies[key].duration} мин</div></div>
+                    <div class="movie__title">${value.movie.title}</div>
+                    <div class="movie__genre">${value.movie.genres.join(', ')}</div>
+                    <div class="movie__duration"><img src="img/clock.png" alt=""><div class="duration__text">${value.movie.duration} мин</div></div>
                 </div>
-                <div class="movie__btn">Подробнее</div>
+                <a class="movie__btn" href="movie.html?movieId=${value.movie.movieId}">Подробнее</a>
             </div>
         </div>
         `;
@@ -78,8 +122,8 @@ function setGenres(){
     let str = '<div class="genre active_genre">Все жанры</div>';
     container.insertAdjacentHTML('beforeend', str);
 
-    for(let genre in genres){
-        str = `<div class="genre">${genres[genre]}</div>`;
+    for(const genre of uniqueGenres){
+        str = `<div class="genre">${genre}</div>`;
         container.insertAdjacentHTML('beforeend', str);
     }
 
@@ -88,11 +132,9 @@ function setGenres(){
         genre.onclick = function(e){
             document.querySelector('.active_genre').classList.toggle("active_genre");
             e.currentTarget.classList.toggle("active_genre");
-            setMovies();
+            setShowtimes();
         }
     });
 }
 
-getMovies();
-setGenres();
-setMovies();
+getShowtimes();
--- js/movie.js
+++ js/movie.js
@@ -1,23 +1,38 @@
 'use strict'
 
-let data = {
-    'title': 'Дюна: Часть вторая',
-    'genres': ['Научная фантастика', 'Боевик'],
-    'duration': 166,
-    'description': 'Пол Атрейдес объединяется с Чани и фрименами, борясь за месть заговорщикам, уничтожившим его семью. ' + 
-    'Стоя перед выбором между любовью всей своей жизни и судьбой известной вселенной, ' +
-    'он стремится предотвратить ужасное будущее, которое может предвидеть только он.'
-};
+let port = 44249;
 
-function setData(){
-    document.querySelector('.description__title').innerHTML = data.title;
-    let genres = document.querySelector('.description__genres');
-    data.genres.forEach(element => {
-        let str = `<div class="description__genre">${element}</div>`;
-        genres.insertAdjacentHTML('beforeend', str);
+async function setData(){
+    let url = `http://127.0.0.1:${port}/api/movies/${getUrlParameter('movieId')}`;
+    let response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        credentials: 'include'
     });
-    document.querySelector('.duration__text').innerHTML = `${data.duration} мин`;
-    document.querySelector('.description__text').innerHTML = data.description;
+    if(response.ok){
+        let body = await response.text();
+        let data = JSON.parse(body);
+
+        let poster = `<img src="${data.poster}" alt="Постер" class="poster__image">`;
+        document.querySelector('.content__poster').innerHTML = '';
+        document.querySelector('.content__poster').insertAdjacentHTML('beforeend', poster);
+        document.querySelector('.description__title').innerHTML = data.title;
+        let genres = document.querySelector('.description__genres');
+        data.genres.forEach(element => {
+            let str = `<div class="description__genre">${element}</div>`;
+            genres.insertAdjacentHTML('beforeend', str);
+        });
+        document.querySelector('.duration__text').innerHTML = `${data.duration} мин`;
+        document.querySelector('.description__text').innerHTML = data.description;
+    }
+    else alert('Ошибка получения фильма');
+}
+
+function getUrlParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
 }
 
 setData();
